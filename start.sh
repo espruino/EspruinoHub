2c4937c3a87a2549900c091dab1c1f4260142a6c
Gordon Williams <gw@pur3.co.uk>
2016-11-25 13:22:25 +0000

    allow auto-restart if it looks like BLE is broken, and disable proxy by default

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ cd `dirname $0`
 # Stop terminal screensaver
 setterm --blank 0
 
-BLENO_ADVERTISING_INTERVAL=300 NOBLE_MULTI_ROLE=1 nodemon index.js
+BLENO_ADVERTISING_INTERVAL=300 NOBLE_MULTI_ROLE=1 node index.js
